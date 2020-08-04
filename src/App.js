af2ab3099cade9d0cd86bee855f3398f8949708f
mumair <mumairofficial@gmail.com>
2020-08-04 22:23:51 +0500

    chore: react-router-dom setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import Header from "./components/Header"
+import CreatePage from "./pages/create/create.page"
+import HomePage from "./pages/home/home.page"
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-    </div>
+
+      <Switch>
+        {/* create page */}
+        <Route path="/create">
+          <CreatePage />
+        </Route>
+
+        {/* home page */}
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
