77d74e8ec3ba4565ac7ff7aad05a8d6e55198a63
mumair <mumairofficial@gmail.com>
2020-08-04 22:21:51 +0500

    chore: designed page header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 function Header() {
-  return <header className="App-header">This is header</header>
+  return (
+    <header class="flex justify-between items-center bg-white shadow-sm p-4">
+      <div class="text-lg font-medium">
+        <Link to="/">
+          <span className="mr-2">⛩</span> Nazim
+        </Link>
+      </div>
+      <div>
+        <Link to="/create" class="px-2 py-1 rounded-lg shadow-sm border-2">
+          create
+        </Link>
+      </div>
+    </header>
+  )
 }
 
 export default Header
